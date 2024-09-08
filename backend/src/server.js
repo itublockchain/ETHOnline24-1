1b06af58d104ce8228b79f7d854f91057c26449e
Emre YAZICI <eemreyazc@NODE201.localdomain>
2024-09-08 03:28:22 +0300

    Soulbound NFT bugs fixed

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,62 +1,48 @@
 import { executeInLit } from "./lit-protocol.js";
-// import { attest } from "./sign";
+import { attest } from "./sign.js";
 
-import express from "express";
-import cors from "cors";
+import { PinataSDK } from "pinata-web3";
+import { Blob } from "buffer";
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import fs from 'fs';
 
 const app = express();
 const port = 4000;
 
-app.use(express.json());
-app.use(cors());
+app.use( express.json() );
+app.use( cors() );
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}.`);
+app.listen(port, () => {console.log(`Server is running on port ${port}.`);});
+
+const pinata = new PinataSDK({
+    pinataJwt: process.env.PINATA_JWT,
+    pinataGateway: process.env.IPFS_GATEWAY,
 });
 
-app.post("/get-analytics", async (req, res) => {
-  const userAddress = req.body.userAddress;
-  console.log(`Analytics of ${userAddress} is calculating..`);
+app.post('/get-analytics', async (req, res) => {
+    
+    const userAddress = req.body.userAddress;
+    console.log(`Analytics of ${userAddress} is calculating..`);
 
-  // const userAnalytics = await executeInLit(userAddress);
+    const userAnalytics = await executeInLit(userAddress);
 
-  // attest(res.score); <- userAddress as input
+    const blob = new Blob([fs.readFileSync("./src/assets/nft.png")]);
+    const file = new File([blob], "image.png", { type: "image/png"})
+    const imageCID = (await pinata.upload.file(file)).IpfsHash;
+    const tokenURI = (await pinata.upload.json({
+        name: "Persona Score NFT",
+        image: `ipfs://${imageCID}`,
+        description: `Total Score: ${userAnalytics.total.score}\n
+                      Mainnet Score: ${userAnalytics.mainnet.score}\n
+                      Arbitrum Score: ${userAnalytics.arbitrum.score}\n
+                      Optimism Score: ${userAnalytics.optimism.score}`,
+    })).IpfsHash;
 
-  
+    await attest(parseInt(userAnalytics.total.score), userAddress, tokenURI);
+    // TODO: score may be float, how to handle this?
 
-  // res.send(userAnalytics);
-  res.json({
-    total: {
-      score: 20,
-      transactions: 232,
-      usdBalance: 40.17177429619103,
-      ethBalance: 0.011044119746717967,
-      erc20Count: 28,
-      nftCount: 203,
-    },
-    mainnet: {
-      score: 52,
-      transactions: 58,
-      usdBalance: 20.964502101660102,
-      ethBalance: 0.00917930983950915,
-      erc20Count: 2,
-      nftCount: 11,
-    },
-    arbitrum: {
-      score: 31,
-      transactions: 111,
-      usdBalance: 2.623963977028003,
-      ethBalance: 0.000766507569032155,
-      erc20Count: 17,
-      nftCount: 100,
-    },
-    optimism: {
-      score: 15,
-      transactions: 63,
-      usdBalance: 16.58330821750293,
-      ethBalance: 0.001098302338176663,
-      erc20Count: 9,
-      nftCount: 92,
-    },
-  });
+    res.send(userAnalytics);
+
 });
--- backend/src/sign.js
+++ backend/src/sign.js
@@ -5,40 +5,35 @@ import 'dotenv/config';
 import fs from 'fs';
 
 const hookContractAddress = JSON.parse(fs.readFileSync('./src/sign-contract/contract-address.json')).address; // MyAttestationHook Contract Address
-const hookContractAbi = JSON.parse(fs.readFileSync('./src/sign-contract/contract-abi.json')).abi; // MyAttestationHook Contract ABI
+// const hookContractAbi = JSON.parse(fs.readFileSync('./src/sign-contract/contract-abi.json')).abi; // MyAttestationHook Contract ABI
 
 const client = new SignProtocolClient(SpMode.OnChain, {
     chain: EvmChains.sepolia,
     account: privateKeyToAccount('0x' + process.env.PRIVATE_KEY),
 });
 
-export async function attest(_score,_recipient) {
+export async function attest(score, recipient, tokenURI) {
     
-    if (_score === undefined) {
+    if (score === undefined) {
         throw new Error("Score undefined");
     }
 
-    // const encodedScore = ethers.utils.defaultAbiCoder.encode(["uint256"], [_score]);
-    // console.log("Encoded Score with ethers.js:", encodedScore);
-    // console.log("Encoded Score Length:", encodedScore.length);
-
     const encodedData = ethers.utils.defaultAbiCoder.encode(
-      ["uint256", "address"],
-      [_score, _recipient]
+        ["uint256", "address", "string"], 
+        [score, recipient, `https://ipfs.io/ipfs/${tokenURI}`]
     );
-    // Schema oluşturma sırasında hook'u belirtin
+
     const schemaInfo = await client.createSchema({
         name: "score",
         data: [{ name: "score", type: "uint256" }],
-        hook: hookContractAddress  // Burada Hook kontrat adresini belirtin
+        hook: hookContractAddress
     });
 
     console.log(schemaInfo);
 
-    // Attestation oluşturma
     const attestationInfo = await client.createAttestation({
         schemaId: schemaInfo.schemaId,
-        data: { score: _score},
+        data: { score: score},
         indexingValue: "score"
     },{
         extraData:encodedData
@@ -47,4 +42,5 @@ export async function attest(_score,_recipient) {
     console.log(attestationInfo);
 
     return attestationInfo.attestationId;
+
 }
